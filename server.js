b91f0747df32bb2905a8edcde3f7c5c0eab0f735
Dominik Wilusz <wilusz1@o2.pl>
2018-10-26 22:45:35 +0200

    Add first pug template

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
 var express = require('express');
 var app = express();
+app.set('view engine', 'pug');
+app.set('views', './views');
 
 
 // middleware
 
-app.use('/store', function(req, res, next) {
-    console.log('Użytkownik kieruje się na stronę /store');
+app.use('/store', function(req, res, next){
+    console.log('Jestem pośrednikiem przy żądaniu do /store');
     next();
 });
 
-
 // app
 
-app.get('/', function(req, res) {
-    res.send('Sklep jest na stronie localhost:3000/store');
+app.get('/', function (req, res) {
+    res.send('Hello world!');
 });
 
-app.get('/store', function(req, res) {
+app.get('/store', function (req, res) {
     res.send('To jest sklep');
+});
+
+app.get('/first-template', function(req, res) {
+    res.render('first-template');
 });
 
 app.listen(3000);
